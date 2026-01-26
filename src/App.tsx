d7bcb976eafcf2fa191f2d5a5eb04a25fa726b6d
Elisangela Amaral <elisangela.amaral9728@gmail.com>
2026-01-26 00:01:09 -0300

    Componente Postagem Finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import Cadastro from './pages/cadastro/Cadastro'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import DeletarTema from './components/tema/deletartema/DeletarTema'
+import ListaPostagens from './components/postagem/listapostagem/ListaPostagem'
+import FormPostagem from './components/postagem/formpostagem/FormPostagem'
+import DeletarPostagem from './components/postagem/deletepostagem/DeletePostagem'
 
 function App() {
 
@@ -25,6 +28,10 @@ return (
             <Route path="/cadastrartema" element={<FormTema />} />
             <Route path="/editartema/:id" element={<FormTema />} />
             <Route path="/deletartema/:id" element={<DeletarTema />} />
+            <Route path="/postagens" element={<ListaPostagens />} />
+            <Route path="/cadastrarpostagem" element={<FormPostagem />} />
+            <Route path="/editarpostagem/:id" element={<FormPostagem />} />
+            <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
           </Routes>
         </div>
         <Footer />
